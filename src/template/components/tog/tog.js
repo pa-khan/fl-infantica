94c5128425c5c6c4faa66f7daf0a8fbff37e855c
Oleg Kiselev <e5ash.bro@gmail.com>
2022-07-29 23:46:21 +0300

    Global fixes

--- src/template/components/tog/tog.js
+++ src/template/components/tog/tog.js
@@ -10,38 +10,38 @@ class Tog {
     this.head = this.element.querySelector('.tog__head');
     this.body = this.element.querySelector('.tog__body');
 
-    this.body.style.transition = this.speed + 'ms';
+    // this.body.style.transition = this.speed + 'ms';
 
-    if (this.element.classList.contains(this.classOpen)) {
-      this.body.style.display = 'block';
-    } else {
-      this.body.style.display = 'none';
-      this.body.style.maxHeight = 0 + 'px';
-    }
+    // if (this.element.classList.contains(this.classOpen)) {
+    //   this.body.style.display = 'block';
+    // } else {
+    //   this.body.style.display = 'none';
+    //   this.body.style.maxHeight = 0 + 'px';
+    // }
 
     this.head.addEventListener('click', () => {
 
       this.element.classList.toggle(this.classOpen);
       this.isOpen = !this.isOpen;
 
-      if (this.isOpen) {
-        this.body.style.display = 'block';
-        this.body.style.maxHeight = this.body.scrollHeight + 'px';
+      // if (this.isOpen) {
+      //   this.body.style.display = 'block';
+      //   this.body.style.maxHeight = this.body.scrollHeight + 'px';
 
-        setTimeout(() => {
-          this.body.removeAttribute('style');
-          this.body.style.transition = this.speed + 'ms';
-        }, this.speed);
-      } else {
-        this.body.style.maxHeight = 0 + 'px';
-        setTimeout(() => {
-          this.body.style.display = 'none';
-          this.body.style.transition = this.speed + 'ms';
-        }, this.speed);
-      }
+      //   setTimeout(() => {
+      //     this.body.removeAttribute('style');
+      //     this.body.style.transition = this.speed + 'ms';
+      //   }, this.speed);
+      // } else {
+      //   this.body.style.maxHeight = 0 + 'px';
+      //   setTimeout(() => {
+      //     this.body.style.display = 'none';
+      //     this.body.style.transition = this.speed + 'ms';
+      //   }, this.speed);
+      // }
 
 
-      console.log(this.body);
+      // console.log(this.body);
     });
   }
 }
